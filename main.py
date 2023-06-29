e9567d0fda34e4f824bd0e2032c5c19dfa808ef2
Danil <danil.komyshev777@gmail.com>
2023-06-29 18:16:55 +0300

    [UPD] made via class

--- main.py
+++ main.py
@@ -1,57 +1,76 @@
 import csv
 import os
 
-
-def load_prefixes(filename):
-    prefixes = {}
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            prefix_zone = row[0]
-            prefixes[row[1]] = prefix_zone
-    return prefixes
+import settings
 
 
-def process_cdr_files(cdr_directory, prefixes_filename, volumes_path):
-    prefixes = load_prefixes(prefixes_filename)
-    volumes = {}
+class MiniBilling:
+    __slots__ = ['cdr_directory', 'prefixes_filename', 'volumes_path', 'volumes']
 
-    cdr_files = os.listdir(cdr_directory)
-    for cdr_file in cdr_files:
-        cdr_path = os.path.join(cdr_directory, cdr_file)
-        with open(cdr_path, 'r') as file:
+    cdr_directory: str
+    prefixes_filename: str
+    volumes_path: str
+
+    volumes: dict
+
+    def __init__(self,
+                 cdr_directory,
+                 prefixes_filename,
+                 volumes_path):
+        self.cdr_directory = cdr_directory
+        self.prefixes_filename = prefixes_filename
+        self.volumes_path = volumes_path
+
+        self.volumes = {}
+
+    @staticmethod
+    def load_prefixes(filename):
+        prefixes = {}
+        with open(filename, 'r') as file:
             reader = csv.reader(file)
-            lines = list(reader)
+            for row in reader:
+                prefix_zone = row[0]
+                prefixes[row[1]] = prefix_zone
+        return prefixes
 
-        for line in lines:
-            msisdn = line[5]
-            dialed = line[6]
-            duration = int(line[8]) if line[8] else 0
+    def process_cdr_files(self):
+        prefixes = self.load_prefixes(self.prefixes_filename)
 
-            msisdn_prefix = max((prefix for prefix in prefixes if msisdn.startswith(prefix)), default='Unknown')
-            dialed_prefix = max((prefix for prefix in prefixes if dialed.startswith(prefix)), default='Unknown')
+        cdr_files = os.listdir(self.cdr_directory)
+        for cdr_file in cdr_files:
+            cdr_path = os.path.join(self.cdr_directory, cdr_file)
+            with open(cdr_path, 'r') as file:
+                reader = csv.reader(file)
+                lines = list(reader)
 
-            line[9] = prefixes.get(msisdn_prefix, 'Unknown')
-            line[10] = prefixes.get(dialed_prefix, 'Unknown')
+            for line in lines:
+                msisdn = line[5]
+                dialed = line[6]
+                duration = int(line[8]) if line[8] else 0
 
-            volumes_key = f"{line[9]},{line[10]}"
-            volumes[volumes_key] = volumes.get(volumes_key, 0) + duration
+                msisdn_prefix = max((prefix for prefix in prefixes if msisdn.startswith(prefix)), default='Unknown')
+                dialed_prefix = max((prefix for prefix in prefixes if dialed.startswith(prefix)), default='Unknown')
 
-        with open(cdr_path, 'w', newline='') as file:
+                line[9] = prefixes.get(msisdn_prefix, 'Unknown')
+                line[10] = prefixes.get(dialed_prefix, 'Unknown')
+
+                volumes_key = f"{line[9]},{line[10]}"
+                self.volumes[volumes_key] = self.volumes.get(volumes_key, 0) + duration
+
+            with open(cdr_path, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(lines)
+
+    def make_volumes(self):
+        with open(self.volumes_path, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerows(lines)
+            for key, value in self.volumes.items():
+                writer.writerow([key, value])
 
-    with open(volumes_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        for key, value in volumes.items():
-            writer.writerow([key, value])
-
-
-resources_path = 'resources'
-cdr_directory = f'{resources_path}/sintetic_data'
-prefixes_filename = f'{resources_path}/prefixes/PREFIXES.TXT'
-volumes_path = f'{resources_path}/volumes/VOLUMES.TXT'
 
 if __name__ == "__main__":
-    process_cdr_files(cdr_directory, prefixes_filename, volumes_path)
-
+    mini_billing = MiniBilling(settings.cdr_directory,
+                               settings.prefixes_filename,
+                               settings.volumes_path)
+    mini_billing.process_cdr_files()
+    mini_billing.make_volumes()
---
+++ settings.py
@@ -0,0 +1,4 @@
+resources_path = 'resources'
+cdr_directory = f'{resources_path}/sintetic_data'
+prefixes_filename = f'{resources_path}/prefixes/PREFIXES.TXT'
+volumes_path = f'{resources_path}/volumes/VOLUMES.TXT'
